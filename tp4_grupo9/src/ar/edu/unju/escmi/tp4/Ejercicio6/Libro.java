055e90017598bf3fd552650e89163571588a9c2f
Jamaledm <Jamalbazan@gmail.com>
2025-09-16 16:28:29 -0300

    FINEJ6

--- tp4_grupo9/src/ar/edu/unju/escmi/tp4/Ejercicio6/Libro.java
+++ tp4_grupo9/src/ar/edu/unju/escmi/tp4/Ejercicio6/Libro.java
@@ -1,10 +1,13 @@
 package ar.edu.unju.escmi.tp4.Ejercicio6;
 
-public class Libro {
+public class Libro implements Comparable<Libro> {
     private String isbn;
     private String titulo;
     private int cantidadPaginas;
     private String autor;
+
+    public Libro() {
+    }
 
     public Libro(String isbn, String titulo, int cantidadPaginas, String autor) {
         this.isbn = isbn;
@@ -13,35 +16,15 @@ public class Libro {
         this.autor = autor;
     }
 
-    // Getters
-    public String getIsbn() {
-        return isbn;
-    }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public int getCantidadPaginas() {
-        return cantidadPaginas;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    // Setters
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setCantidadPaginas(int cantidadPaginas) {
-        this.cantidadPaginas = cantidadPaginas;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
+    public String getIsbn() { return isbn; }
+    public String getTitulo() { return titulo; }
+    public int getCantidadPaginas() { return cantidadPaginas; }
+    public String getAutor() { return autor; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public void setCantidadPaginas(int cantidadPaginas) { this.cantidadPaginas = cantidadPaginas; }
+    public void setAutor(String autor) { this.autor = autor; }
 
     public void mostrarDatos() {
         System.out.println("ISBN: " + isbn);
@@ -49,5 +32,10 @@ public class Libro {
         System.out.println("Cantidad de páginas: " + cantidadPaginas);
         System.out.println("Autor: " + autor);
         System.out.println("-----------------------------");
+    }
+
+    @Override
+    public int compareTo(Libro otro) {
+        return this.titulo.compareToIgnoreCase(otro.titulo);
     }
 }
