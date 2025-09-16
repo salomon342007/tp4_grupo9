8197108d9716930cbfe040f4b01b76bfb09d8298
Jamaledm <Jamalbazan@gmail.com>
2025-09-16 14:59:04 -0300

    ejercicio4primerparte

--- tp4_grupo9/src/ar/edu/unju/escmi/tp4/Ejercicio4/ejercicio4.java
+++ tp4_grupo9/src/ar/edu/unju/escmi/tp4/Ejercicio4/ejercicio4.java
@@ -1,5 +1,17 @@
 package ar.edu.unju.escmi.tp4.Ejercicio4;
 
+import java.util.HashSet;
+
 public class ejercicio4 {
-    
+    public static void main(String[] args) {
+        HashSet<Cliente> clientes = new HashSet<>();
+
+        clientes.add(new Cliente("12345678", "Juan Perez", "frecuente"));
+        clientes.add(new Cliente("87654321", "Ana Gomez", "ocasional"));
+        clientes.add(new Cliente("11223344", "Carlos Ruiz", "frecuente"));
+
+        for (Cliente c : clientes) {
+            System.out.println(c);
+        }
+    }
 }
